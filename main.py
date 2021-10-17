bc42da76ea013e75a47a406249dac96346cf7b9b
Flippergail <ticarter11@icloud.com>
2021-10-17 21:59:39 +0100

    building chess engine skeleton

--- main.py
+++ main.py
@@ -1,31 +1,91 @@
 # importing packages
 import pandas as pd
 import numpy as np
-import random
+from evaluationModule import evaluate
+from evaluationModule import checkGameEnd
 import chess
+from math import pi
 
 board = chess.Board()
-#if board.is_checkmate() or board.is_stalemate() or board.is_fivefold_repetition() or board.is_seventyfive_moves() or board.is_insufficient_material():
-# evaluation function
-def evaluate(board_position):
-    bp = board_position
-    points = random.randint(1, 10)
-    return points
+runamount = 0
+moveNumber = 0
 
-# tree traversing function
-def traverse_func(board, remaining_depth):
-    if remaining_depth == 0:
-        my_moves = pd.DataFrame(data={'score', 'move'})
-        for i, v in enumerate(list(board.legal_moves)):
-            board.push(chess.Move.from_uci(str(v)))
-            enemy_moves = pd.DataFrame([[0, 0]], columns=['score', 'move'])
-            print(enemy_moves)
-            for z, k in enumerate(list(board.legal_moves)):
-                board.push(chess.Move.from_uci(str(k)))
-                score = evaluate(board)
-                enemy_moves.iat[z, 0] = score
-                enemy_moves.iat[z, 1] = str(k)
-                board.pop()
+# negamax search
+
+
+def negaMax(board, depth):
+    global runamount
+    runamount += 1
+    if depth == 0: return evaluate(board)
+    max = -float('inf')
+    for i, v in enumerate(board.legal_moves):
+        board.push(chess.Move.from_uci(str(v)))
+        score = -negaMax(board, depth-1)
+        board.pop()
+        if score > max:
+            max = score
+    return max
+
+
+def evalMoves(board, depth):
+    global moveNumber
+    moveNumber += 1
+    if checkGameEnd(board) != pi:
+        print("Game Has Ended.")
+        print(moveNumber)
+        exit()
+    my_moves = pd.DataFrame(columns=['score', 'move'])
+    my_moves['row_num'] = np.arange(len(list(board.legal_moves)))
+    for i, v in enumerate(board.legal_moves):
+        board.push(chess.Move.from_uci(str(v)))
+        if depth == 0: score = evaluate(board)
+        elif checkGameEnd(board) != pi: score = checkGameEnd(board)
+        else: score = negaMax(board, depth-1)
+        board.pop()
+        my_moves.iat[i, 0] = score
+        my_moves.iat[i, 1] = str(v)
+    best_move = my_moves.loc[my_moves['score'].astype(float).idxmin()]['move']
+    return best_move
+
+
+"""def traverse_func(board, remaining_depth):
+    global runamount
+    runamount += 1
+    remaining_depth -= 1
+    my_moves = pd.DataFrame(columns=['score', 'move'])
+    my_moves['row_num'] = np.arange(len(list(board.legal_moves)))
+    for i, v in enumerate(board.legal_moves):
+        board.push(chess.Move.from_uci(str(v)))
+        enemy_moves = pd.DataFrame(columns=['score', 'move'])
+        enemy_moves['row_num'] = np.arange(len(list(board.legal_moves)))
+        for z, k in enumerate(board.legal_move
+        s):
+            board.push(chess.Move.from_uci(str(k)))
+            if remaining_depth == 1:
+                print("miss")
+                enemy_score = evaluate(board)
+            else:
+                enemy_score = traverse_func(board, remaining_depth)
+            enemy_moves.iat[z, 0] = enemy_score
+            enemy_moves.iat[z, 1] = str(k)
             board.pop()
-            print(enemy_moves)
-traverse_func(board, 0)
+        enemy_row = enemy_moves.loc[enemy_moves['score'].astype(float).idxmax()]
+        my_moves.iat[i, 0] = enemy_row['score']
+        my_moves.iat[i, 1] = enemy_row['move']
+        board.pop()
+    return enemy_moves.loc[enemy_moves['score'].astype(float).idxmin()]['move']"""
+
+while not board.is_game_over():
+    move = evalMoves(board, 3)
+    board.push(chess.Move.from_uci(move))
+    print(move)
+    print(moveNumber)
+    playerMove = input("make a move ")
+    if playerMove == "board":
+        print(board)
+        board.pop()
+        board.pop()
+        playerMove = input("make a move ")
+    board.push(chess.Move.from_uci(playerMove))
+    moveNumber += 1
+    print(moveNumber)
